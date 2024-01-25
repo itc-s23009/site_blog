97de300b8adbf7df3aa1daf81bb51c2670dc2e7e
itc-s23009 <s23009@std.it-college.ac.jp>
2024-01-25 06:37:06 +0000

    4.5 fin Containerコンポーネントで横幅を整える container.jsを作成 container.module.cssを作成

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,11 @@
+import Container from 'components/Container'
 import Hero from 'components/hero'
 
 const Blog = () => {
-  return <Hero title='Blog' subtitle='Recent Posts' />
+  return (
+    <Container>
+      <Hero title='Blog' subtitle='Recent Posts' />
+    </Container>
+  )
 }
 export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 const Home = () => {
   return (
-    <>
+    <Container>
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
-    </>
+    </Container>
   )
 }
 export default Home
