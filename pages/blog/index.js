efb10e5fbaee5e5c7d97c672c289770863efc016
itc-s23009 <s23009@std.it-college.ac.jp>
2024-02-08 02:04:09 +0000

    9.6 fin 記事一覧にnext/imageでアイキャッチ画像を追加する

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { getImageBuffer } from 'lib/getlmageBuffer'
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -19,6 +22,15 @@ export default Blog
 
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
